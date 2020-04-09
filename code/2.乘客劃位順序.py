72295277d3a7abd8aec4e48f8a1c7761ac0434b7
luke90329 <32509228+luke90329@users.noreply.github.com>
2020-04-09 16:07:44 +0800

    Add files via upload

--- code/2.乘客劃位順序.py
+++ code/2.乘客劃位順序.py
@@ -2,10 +2,7 @@ s = []
 for i in range(eval(input())):
     a, b, c, d = map(str,input().split())
     p = (int(b)/1000) + int(c) - int(d)
-    s.append([a,int(p)])
-for i in range(len(s)):
-    for j in range(len(s)):
-        if s[i][1] < s[j][1]:
-            s[i], s[j] = s[j], s[i]
+    s.append((a,int(p)))
+s.sort(key=lambda x:x[1])
 for i in range(len(s)):
     print(s[i][0], s[i][1])
